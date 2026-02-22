3085fdf8816511d91592c9320205634219cfeeb2
S19nikhilesh <nikhileshsharma2005@gmail.com>
2026-02-22 16:01:49 +0530

    basic APIs for DSA problem creation

--- src/routes/problemCreator.js
+++ src/routes/problemCreator.js
@@ -11,3 +11,30 @@ problemRouter.get("/:id",problemFetch);
 problemRouter.get("/",problemFetchAll);
 
 problemRouter.get("/user",solvedProblem)
+
+const express=require('express');
+const problemRouter=express.Router();
+
+//admin access required for these 3
+problemRouter.post("/create",problemCreate);
+problemRouter.patch("/:id",problemUpdate);
+problemRouter.delete("/:id",probleemDelete);
+
+//user access
+problemRouter.get("/:id",problemFetch);
+problemRouter.get("/",problemFetchAll);
+
+problemRouter.get("/user",solvedProblem)
+const express=require('express');
+const problemRouter=express.Router();
+
+//admin access required for these 3
+problemRouter.post("/create",problemCreate);
+problemRouter.patch("/:id",problemUpdate);
+problemRouter.delete("/:id",probleemDelete);
+
+//user access
+problemRouter.get("/:id",problemFetch);
+problemRouter.get("/",problemFetchAll);
+
+problemRouter.get("/user",solvedProblem)
