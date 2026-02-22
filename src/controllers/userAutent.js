80263d044bff5bc52d42640e17e92511446636a8
S19nikhilesh <nikhileshsharma2005@gmail.com>
2026-02-22 15:21:22 +0530

    admin register

--- src/controllers/userAutent.js
+++ src/controllers/userAutent.js
@@ -13,11 +13,11 @@ const register= async(req,res)=>{
         const {firstName,emailId,password}=req.body;
         //bina hashing ke password store hota hai kya be? 
         req.body.password=await bcrypt.hash(password,10);
-        
+        req.body.role="user";
         //if email alredy exixts yeh khud hi error fenk dega    
         const user= await User.create(req.body);
         //token bhi generate karwa de jwt.sign({emailId},"secet_key",{expiresIn: 60*60});
-        const token=jwt.sign({_id:user._id,emailId:emailId},process.env.JWT_KEY,{expiresIn: 60*60});
+        const token=jwt.sign({_id:user._id,emailId:emailId,role:'user'},process.env.JWT_KEY,{expiresIn: 60*60});
         res.cookie("token",token,{maxAge: 60*60*1000 });
         res.status(201).send("User Registered Successfully");
         //new resource created status:201
@@ -43,7 +43,7 @@ const login=async(req,res)=>{
         if(!match)
             throw new Error("Invalid Credentials");
 
-        const token=jwt.sign({_id:user._id,emailId:emailId},process.env.JWT_KEY,{expiresIn: 60*60});
+        const token=jwt.sign({_id:user._id,emailId:emailId,role:user.role},process.env.JWT_KEY,{expiresIn: 60*60});
         res.cookie("token",token,{maxAge: 60*60*1000 });
         res.status(200).send("User Login Successfully"); //ok
 
@@ -73,5 +73,27 @@ const logout=async(req,res)=>{
     }
 }
 
-module.exports={register,login,logout}
+const adminRegister=async(req,res)=>{
+    try{
+
+        //valdiate toh karway ahi nhi , pehle validator banao 
+        validate(req.body)
+
+        const {firstName,emailId,password}=req.body;
+        //bina hashing ke password store hota hai kya be? 
+        req.body.password=await bcrypt.hash(password,10);
+        req.body.role="admin";
+        //if email alredy exixts yeh khud hi error fenk dega    
+        const user= await User.create(req.body);
+        //token bhi generate karwa de jwt.sign({emailId},"secet_key",{expiresIn: 60*60});
+        const token=jwt.sign({_id:user._id,emailId:emailId,role:'user'},process.env.JWT_KEY,{expiresIn: 60*60});
+        res.cookie("token",token,{maxAge: 60*60*1000 });
+        res.status(201).send("User Registered Successfully");
+        //new resource created status:201
+
+    }catch(err){
+        res.status(400).send("Error:"+err);// status code 400: bad request 
+    }
+}
+module.exports={register,login,logout,adminRegister}
 
--- src/routes/userAuth.js
+++ src/routes/userAuth.js
@@ -1,12 +1,13 @@
 const express=require('express');
 const authRouter=express.Router();
-const {register,login,logout}=require("../controllers/userAutent")
+const {register,login,logout,adminRegister}=require("../controllers/userAutent")
 const userMiddleware=require('../middlewares/userMiddleware')
+const adminMiddleware=require('../middlewares/adminMiddleware')
 
 authRouter.post('/register',register);
 authRouter.post('/login',login);
 authRouter.post('/logout',userMiddleware,logout);
-// authRouter.post('/admin/register',adminRegister)
+authRouter.post('/admin/register',adminMiddleware,adminRegister)
 // authRouter.get('/getprofile',getprofile);
 
 module.exports= authRouter;
