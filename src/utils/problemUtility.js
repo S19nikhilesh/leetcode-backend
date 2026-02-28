9cefb1f722e5aec10b16844fb0a50b01e29c4446
S19nikhilesh <nikhileshsharma2005@gmail.com>
2026-02-28 10:14:13 +0530

    execute code using JDOODLE API

---
+++ src/utils/problemUtility.js
@@ -0,0 +1,43 @@
+const axios = require("axios");
+
+const getLanguageName = (lang) => {
+
+  if (!lang) {
+    throw new Error("Language is missing");
+  }
+    const languageMap = {
+      "c++": "cpp",
+      "c": "c",
+      "python": "python3",
+      "java": "java"
+    };
+  
+    return languageMap[lang.toLowerCase()];
+};
+
+
+
+
+const executeCode = async (combinedInput, language, code) => {
+  // console.log(language,combinedInput,code)
+  const response = await axios.post(
+    "https://api.jdoodle.com/v1/execute",
+    {
+      // clientId: process.env.JDOODLE_CLIENT_ID,
+      // clientSecret: process.env.JDOODLE_CLIENT_SECRET,
+      clientId: "ddb0ab0e35c3db904124de75369028af",
+      clientSecret: "96691beb865d06bd45dac9e1bff4bd086941b9c5a2c2bfb965e7c5dc65839813",
+      script: getDriverTemplate(language,code),  
+      stdin: combinedInput,
+      language: getLanguageName(language),
+      versionIndex: "4"
+    }
+    
+  );
+ console.log(getLanguageName(language))
+  return response.data;
+};
+
+module.exports = executeCode ;
+
+
