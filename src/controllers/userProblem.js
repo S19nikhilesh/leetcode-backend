de44c3290c8a4ec06f8d23dbf3f28cfade5c0c38
S19nikhilesh <nikhileshsharma2005@gmail.com>
2026-02-28 12:07:19 +0530

    delete Existing Problem API added

--- src/controllers/userProblem.js
+++ src/controllers/userProblem.js
@@ -94,7 +94,21 @@ const updateProblem=async(req,res)=>{
         res.status(404).send( "Error:" +err);
     }
 }
+const deleteProblem=async(req,res)=>{
+    const {id}=req.params;
+    try{
+        if(!id){
+            res.status(400).send("Missing Id Field");
+            const deletedProblem=Problem.findByIdAndDelete(id);
 
+            if(!deletedProblem)
+                return res.status(404).send("Problem not found")
+            res.status(200).send("Problem Deleted");
+        }
+    }catch(err){
+        res.status(500).send("Error:"+err)
+    }
+}
 
 
 module.exports={createProblem,updateProblem,deleteProblem};
