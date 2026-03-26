c869bd82fb28854836e362ba98301838a3cb3e68
S19nikhilesh <nikhileshsharma2005@gmail.com>
2026-03-26 15:25:21 +0530

    show and hide password usign EYE icon added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   const {isAuthenticated}=useSelector((state)=>state.auth) //state. slice ka naam
 
   const dispatch=useDispatch();
-  
+
   useEffect(()=>{
     dispatch(checkAuth()); //function call
   },[dispatch])//ek hi baar call krna chchta hu , jab dispatch change ho -jo kabhi hoga nhi,ya khali array bhi chodd skte ho
@@ -27,6 +27,7 @@ function App() {
         <Route path="/" element={isAuthenticated?<Homepage></Homepage>:<Navigate to="/signup"/>}></Route>
         <Route path="/login" element={isAuthenticated?<Navigate to="/"/> :<Login></Login>}></Route>
         <Route path="/signup" element={isAuthenticated?<Navigate to="/"/> :<Signup></Signup>}></Route>
+        {/* <Route path=" "></Route> */}
         {/* 
         <Route path="/"><Homepage></Homepage></Route>
         <Route path="/login" ><Login></Login></Route>
